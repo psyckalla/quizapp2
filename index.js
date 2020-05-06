7f21c0d1b3719670c1cb5c13caec10cd3db800a9
Katerina <psyckalla@gmail.com>
2020-05-06 19:03:45 -0400

    second commit

--- index.js
+++ index.js
@@ -12,24 +12,103 @@ options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves
 correct: "Anne of Cleves"},
 {question: "Which wife did Henry VIII execute?",
 options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
-correct: "Anne of Cleves"},
+correct: "Anne of Cleves"}, 
 questionNumber: 0,
 storedScore: 0];
 
 //start the quiz
 
+function startQuiz() {
+    const startButton = $(.start-button)
+    $(startButton.on('click', function (event) {
+        for (let i = 0; i < 5; i++) {
+            let currentSTORE = STORE[i];
+            let insertHTML = $(`<div class="question-form">
+                <p>${currentSTORE.question}</p>
+                <ul>
+                    <li>${currentStore.options[0]}</li>
+                    <li>${currentStore.options[1]}</li>
+                    <li>${currentStore.options[2]}</li>
+                    <li>${currentStore.options[3]}</li>
+            </div>)`
 
-//update score and question number
+            $(".main").html(insertHTML)
+            )
+            )
+            );
+        };
+
+    });
+};
+
+
+//update score 
+
+function updateScore() {
+
+};
+
+
+//update question number
+
+function updateQuestionNumber() {
+    store.questionNumber++;
+
+};
+
 
 
 //present the question, and the next button
 
+function presentQuestion() {
+
+};
+
 
 //question is correct
+
+function correctQuestion() {
+
+};
 
 
 //question is incorrect
 
+function incorrectQuestion() {
+
+};
+
 
 //passed the quiz
 
+function passedQuiz() {
+
+};
+
+
+//failed the quiz
+
+function failedQuiz() {
+
+};
+
+
+//reset the quiz
+
+function resetQuiz() {
+
+};
+
+function generateQuiz {
+    startQuiz();
+    resetQuiz();
+    failedQuiz();
+    passedQuiz();
+    incorrectQuestion();
+    correctQuestion();
+    presentQuestion();
+    updateQuestionNumber();
+    updateScore();
+};
+
+$(generateQuiz);
