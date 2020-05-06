116fee58d84ba41a00de98af011ec639ea99947d
Katerina <psyckalla@gmail.com>
2020-05-06 18:23:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+STORE = [{question: "How many wives did Henry VIII have?", 
+options: [4, 1, 8, 6], 
+correct: 6}, 
+{question: "Henry VIII was succeeded by:", 
+options: ["Mary I of England", "Edward VI of England", "Elizabeth I of England", "Mary Queen of Scots"], 
+correct: "Edward VI of England"}, 
+{question: "Henry VIII annuled his marriage to:", 
+options: ["Catherine of Aragorn", "Jane Seymour", "Anne Boleyn", "Anne of Cleves"], 
+correct: "Catherine of Aragorn"},
+{question: "Which of Henry VIII's wives gave birth to his heir?",
+options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
+correct: "Anne of Cleves"},
+{question: "Which wife did Henry VIII execute?",
+options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
+correct: "Anne of Cleves"},
+questionNumber: 0,
+storedScore: 0];
+
+//start the quiz
+
+
+//update score and question number
+
+
+//present the question, and the next button
+
+
+//question is correct
+
+
+//question is incorrect
+
+
+//passed the quiz
+
