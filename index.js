de4a9874294a8b4f72927113d9ee11358fe61b8d
Katerina <psyckalla@gmail.com>
2020-05-07 01:26:56 -0400

    so many changes

--- index.js
+++ index.js
@@ -15,17 +15,31 @@ correct: "Anne of Cleves"},
 {question: "Which wife did Henry VIII execute?",
 options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
 correct: "Anne of Cleves"}, 
-{questionNumber: 0}, 
+{totalQuestion: 0}, 
 {storedScore: 0}];
 
-//start the quiz
+//hide start button
 
-function startQuiz() {
+function hideStartButton() {
+    $(".start-button").on('click', function (event) {
+        $(".start-button").hide();
+
+
+    });
     
-
-
-    console.log('start quiz ran');
+    
+    console.log('hidestartbutton ran');
+    console.log($('.question-number'));
 };
+
+
+// start the quiz
+function startQuiz() {
+    hideStartButton();
+    console.log('startquiz ran');
+
+};
+
 
 
 //update score 
@@ -37,28 +51,61 @@ function updateScore() {
 };
 
 
-//update question number
-
-function updateQuestionNumber() {
-
-    console.log('updatequestionnumber ran');
-
-};
-
-
 
 //present the question, and the next button
 
 function presentQuestion() {
-
+    
+    let i = 0;
+    if (i < 5) {
+    $('.start-button, .next-button').on('click', function(event) {
+    $('.main').html('<p>' + STORE[i].question + '</p>' + 
+    '<input name="test" type="radio" value="inc" />' + STORE[i].options[0] +
+    '<input name="test" type="radio" value="inc" />' + STORE[i].options[1] +
+    '<input name="test" type="radio" value="inc" />' + STORE[i].options[2] +
+    '<input name="test" type="radio" value="inc" />' + STORE[i].options[3]);
+    $('input[name="test"]').on('click', function() {
+        if ($(this).val() === STORE[i].correct) {
+             ('.main').html('BOOYAH');
+             $('.start-button').html('booyah')
+        } else {
+             
+            }
+        });
+    $('.question-number').html(STORE[5].totalQuestion+=1);
+    
+    i += 1;
     console.log('presentquestion ran');
+    });
+} else if (i === 5) {
+    this.reload(true);
+}
+};
 
+
+
+//next question
+
+function nextQuestion() {
+    $('.next-button', '.start-button').on('click', function(event) {
+        $('.main').html('<p>' + STORE[STORE[5].totalQuestion].question + '</p>' +
+        '<button class="next-button">Next</button>');
+        $('.question-number').html(STORE[5].totalQuestion+=1);
+    });
 };
 
 
 //question is correct
 
 function correctQuestion() {
+    $('input[name="test"]').on('click', function() {
+        if ($(this).val() === STORE[i].correct) {
+             ('.main').html('BOOYAH')
+        } else {
+             
+            }
+        })
+        
 
     console.log('correctquestion ran');
 
@@ -90,7 +137,7 @@ function failedQuiz() {
     console.log('failedquiz ran');
 
 };
-
+ 
 
 //reset the quiz
 
@@ -101,15 +148,8 @@ function resetQuiz() {
 };
 
 function generateQuiz() {
-    startQuiz();
-    resetQuiz();
-    failedQuiz();
-    passedQuiz();
-    incorrectQuestion();
-    correctQuestion();
     presentQuestion();
-    updateQuestionNumber();
-    updateScore();
+    correctQuestion();
 };
 
 $(generateQuiz());
