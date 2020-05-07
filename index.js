dab806bf9f0c50492f14601a866fd9ff31583801
Katerina <psyckalla@gmail.com>
2020-05-07 11:07:19 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 'use strict'
 
 const STORE = [{question: "How many wives did Henry VIII have?", 
-options: [4, 1, 8, 6], 
-correct: "D"}, 
+options: ["A) 4", "B) 1", "C) 8", "D) 6"], 
+correct: "A"}, 
 {question: "Henry VIII was succeeded by:", 
-options: ["Mary I of England", "Edward VI of England", "Elizabeth I of England", "Mary Queen of Scots"], 
+options: ["A) Mary I of England", "B) Edward VI of England", "C) Elizabeth I of England", "D) Mary Queen of Scots"], 
 correct: "B"},
 {question: "Henry VIII annuled his marriage to:", 
-options: ["Catherine of Aragorn", "Jane Seymour", "Anne Boleyn", "Anne of Cleves"], 
+options: ["A) Catherine of Aragorn", "B) Jane Seymour", "C) Anne Boleyn", "D) Anne of Cleves"], 
 correct: "A"},
 {question: "Which of Henry VIII's wives gave birth to his heir?",
-options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
+options: ["A) Catherine of Aragorn", "B) Anne Boleyn", "C) Jane Seymour", "D) Anne of Cleves"],
 correct: "C"},
 {question: "Which wife did Henry VIII execute?",
-options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
-correct: "B"}, 
-{totalQuestion: 0}];
+options: ["A) Catherine of Aragorn", "B) Anne Boleyn", "C) Jane Seymour", "D) Anne of Cleves"],
+correct: "B"}];
 
 //hide start button
 
@@ -26,86 +25,75 @@ function hideStartButton() {
     })
 };
 
-
+let i = 0;
+let correctScore = 0;
+let incorrectScore = 0;
 //present the question, and the next button
 
 function presentQuestion() {
-    let i = 0;
-    let correctScore = 0;
-    let incorrectScore = 0;
-    
-    $('.start-button, .next-button').on('click', function(event) {
-        $('.main').html(`<p>${STORE[i].question}</p><input name="test" type="radio" value="A" />${STORE[i].options[0]}<input name="test" type="radio" value="B" />${STORE[i].options[1]}<input name="test" type="radio" value="C" />${STORE[i].options[2]}<input name="test" type="radio" value="D" />${STORE[i].options[3]}`);
 
+    
+
+        $('.start-button, .next-button').on('click', function(event) {
+            $('.js-answer').empty();
+            
+            if (i < 5) {
+                $('.main').html(`<p>${STORE[i].question}</p><input name="test" type="radio" value="A" />${STORE[i].options[0]}<input name="test" type="radio" value="B" />${STORE[i].options[1]}<input name="test" type="radio" value="C" />${STORE[i].options[2]}<input name="test" type="radio" value="D" />${STORE[i].options[3]}`);
+                i++;
+            } else {
+                    $('.main').html(`<p>DONE</p>`);
+                    $('.next-button').addClass('hidden');
+                    $('.test-score-button').removeClass('hidden');
+            };
             $('input[name="test"]').on('click', function() {
                 if ($(this).val() == STORE[i-1].correct) {
+                    $('.js-answer').html('You are correct!');
                     correctScore += 1;
              } else {
+                $('.js-answer').html(`You are incorrect, the correct answer is ${STORE[i-1].correct}`);
                  incorrectScore += 1;
-
             };
         });
-        $('.question-number').html(i + 1);
+        $('.question-number').html(i);
         $('.stored-scored').html(correctScore);
         $('.stored-incorrect').html(incorrectScore);
-        i++;
+        
     });
 
-};
     
 
 
 
-
-
-//question is correct
-
-function correctQuestion() {
-        
-
-    console.log('correctquestion ran');
-
 };
 
-
-//question is incorrect
-
-function incorrectQuestion() {
-
-    console.log('incorrectquestion ran');
-
-};
 
 
 //passed the quiz
 
-function passedQuiz() {
-
-    console.log('passedquiz ran');
-
-};
-
-
-//failed the quiz
-
-function failedQuiz() {
-
-    console.log('failedquiz ran');
-
+function quizResults () {
+    $('.test-score-button').on('click', function(event) {
+        const results = (correctScore / i) * 100;
+        $('.main').html(`Your final grade is: ${results}%`);
+        $('.test-score-button').addClass('hidden');
+        $('.reset').removeClass('hidden');
+        
+    });
 };
  
 
 //reset the quiz
 
 function resetQuiz() {
-
-    console.log('resetquiz ran');
-
+    $('.reset').on('click', function (event) {
+        location.reload(true);
+    });
 };
 
 function generateQuiz() {
     hideStartButton();
     presentQuestion();
+    quizResults();
+    resetQuiz();
 };
 
 $(generateQuiz());
