f9b56ec8ff6c056210e6e1a5e63cfa44b8ca603d
Katerina <psyckalla@gmail.com>
2020-05-07 08:53:16 -0400

    working correct incorrect

--- index.js
+++ index.js
@@ -2,109 +2,65 @@
 
 const STORE = [{question: "How many wives did Henry VIII have?", 
 options: [4, 1, 8, 6], 
-correct: 6}, 
+correct: "D"}, 
 {question: "Henry VIII was succeeded by:", 
 options: ["Mary I of England", "Edward VI of England", "Elizabeth I of England", "Mary Queen of Scots"], 
-correct: "Edward VI of England"},
+correct: "B"},
 {question: "Henry VIII annuled his marriage to:", 
 options: ["Catherine of Aragorn", "Jane Seymour", "Anne Boleyn", "Anne of Cleves"], 
-correct: "Catherine of Aragorn"},
+correct: "A"},
 {question: "Which of Henry VIII's wives gave birth to his heir?",
 options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
-correct: "Anne of Cleves"},
+correct: "C"},
 {question: "Which wife did Henry VIII execute?",
 options: ["Catherine of Aragorn", "Anne Boleyn", "Jane Seymour", "Anne of Cleves"],
-correct: "Anne of Cleves"}, 
-{totalQuestion: 0}, 
-{storedScore: 0}];
+correct: "B"}, 
+{totalQuestion: 0}];
 
 //hide start button
 
 function hideStartButton() {
-    $(".start-button").on('click', function (event) {
-        $(".start-button").hide();
-
-
-    });
-    
-    
-    console.log('hidestartbutton ran');
-    console.log($('.question-number'));
+    $('.start-button').on('click', function(event) {
+        $('.next-button').removeClass('hidden');
+        $('.start-button').addClass('hidden');
+    })
 };
-
-
-// start the quiz
-function startQuiz() {
-    hideStartButton();
-    console.log('startquiz ran');
-
-};
-
-
-
-//update score 
-
-function updateScore() {
-
-    console.log('updatescore ran');
-
-};
-
 
 
 //present the question, and the next button
 
 function presentQuestion() {
-    
     let i = 0;
-    if (i < 5) {
-    $('.start-button, .next-button').on('click', function(event) {
-    $('.main').html('<p>' + STORE[i].question + '</p>' + 
-    '<input name="test" type="radio" value="inc" />' + STORE[i].options[0] +
-    '<input name="test" type="radio" value="inc" />' + STORE[i].options[1] +
-    '<input name="test" type="radio" value="inc" />' + STORE[i].options[2] +
-    '<input name="test" type="radio" value="inc" />' + STORE[i].options[3]);
-    $('input[name="test"]').on('click', function() {
-        if ($(this).val() === STORE[i].correct) {
-             ('.main').html('BOOYAH');
-             $('.start-button').html('booyah')
-        } else {
-             
-            }
-        });
-    $('.question-number').html(STORE[5].totalQuestion+=1);
+    let correctScore = 0;
+    let incorrectScore = 0;
     
-    i += 1;
-    console.log('presentquestion ran');
+    $('.start-button, .next-button').on('click', function(event) {
+        $('.main').html(`<p>${STORE[i].question}</p><input name="test" type="radio" value="A" />${STORE[i].options[0]}<input name="test" type="radio" value="B" />${STORE[i].options[1]}<input name="test" type="radio" value="C" />${STORE[i].options[2]}<input name="test" type="radio" value="D" />${STORE[i].options[3]}`);
+
+            $('input[name="test"]').on('click', function() {
+                if ($(this).val() == STORE[i-1].correct) {
+                    correctScore += 1;
+             } else {
+                 incorrectScore += 1;
+
+            };
+        });
+        $('.question-number').html(i + 1);
+        $('.stored-scored').html(correctScore);
+        $('.stored-incorrect').html(incorrectScore);
+        i++;
     });
-} else if (i === 5) {
-    this.reload(true);
-}
+
 };
+    
 
 
 
-//next question
-
-function nextQuestion() {
-    $('.next-button', '.start-button').on('click', function(event) {
-        $('.main').html('<p>' + STORE[STORE[5].totalQuestion].question + '</p>' +
-        '<button class="next-button">Next</button>');
-        $('.question-number').html(STORE[5].totalQuestion+=1);
-    });
-};
 
 
 //question is correct
 
 function correctQuestion() {
-    $('input[name="test"]').on('click', function() {
-        if ($(this).val() === STORE[i].correct) {
-             ('.main').html('BOOYAH')
-        } else {
-             
-            }
-        })
         
 
     console.log('correctquestion ran');
@@ -148,8 +104,8 @@ function resetQuiz() {
 };
 
 function generateQuiz() {
+    hideStartButton();
     presentQuestion();
-    correctQuestion();
 };
 
 $(generateQuiz());
